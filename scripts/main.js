2b5d09dd1842bb21e3e5dd5384327c1b317baf1c
Squeeshy123 <62818641+Squeeshy123@users.noreply.github.com>
2024-01-26 12:23:05 -0500

    made a button function yay!

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
-alert("HAIIIIII :3 <3")
+window.onload = init;
+
+var box 
+var text_box
+
+function init(){
+    box = document.getElementById("options_box")
+    text_box = document.getElementById("user_input")
+}
+
+
+function submit_clicked() {
+    create_option(text_box.value)
+}
+
+function create_option(button_text){
+    var new_option = document.createElement("button")
+    box.appendChild(new_option)
+    new_option.innerText = button_text
+}
+
