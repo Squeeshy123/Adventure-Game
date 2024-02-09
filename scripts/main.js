b89b613bf5858a75e78bc24e2979eb060f930ad6
Squeeshy123 <62818641+Squeeshy123@users.noreply.github.com>
2024-02-09 12:10:11 -0500

    made a text box

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,18 @@
 window.onload = init;
 
-varoptions_box 
+var options_box 
 var text_box
 
 function init(){
     options_box = document.getElementById("options_box")
     text_box = document.getElementById("user_input")
+    adventure_log = document.getElementById("adventure_log")
 }
 
 
 function submit_clicked() {
     create_option(text_box.value)
+    print_line_to_log(text_box.value)
 }
 
 function create_option(button_text){
@@ -21,6 +23,13 @@ function create_option(button_text){
 
 function clear_options(){
     
+}
+
+function print_to_log(text) {
+    adventure_log.innerHTML += text
+}
+function print_line_to_log(text) {
+    adventure_log.innerHTML += text + "\n"
 }
 
 // clear optionoptions_box
